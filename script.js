d6c3663991cb12e8d7c6dcf62a21624bc3e760e5
AndreRichartz <andrerichartz07@gmail.com>
2023-11-27 20:18:48 -0300

    side bar done

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
     const button = document.getElementById('menu-icon');
     button.addEventListener("click", () => {
-        const testeElement = document.querySelector('.teste');
-        if (testeElement.style.left === '-1000px') { 
-            testeElement.style.left = '0px';
-        } else {
-            testeElement.style.left = '-1000px';
-        }
+        const sideBarElement = document.getElementById('side-bar');
+        sideBarElement.classList.toggle('open');
+        const isOpen = sideBarElement.classList.contains('open')
+        console.log('hello world')
     });
+    
 });
